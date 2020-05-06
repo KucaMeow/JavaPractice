580e78e6d448d28e736d969f9b65478cc4900d03
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2020-05-06 20:18:47 +0300

    Custom Scope

--- semestrovaya/src/main/java/ru/itis/semestrovaya/config/ApplicationConfig.java
+++ semestrovaya/src/main/java/ru/itis/semestrovaya/config/ApplicationConfig.java
@@ -1,14 +1,21 @@
 package ru.itis.semestrovaya.config;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.itis.semestrovaya.scope.UserScopePostProcessor;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor beanFactoryPostProcessor() {
+        return new UserScopePostProcessor();
     }
 }
