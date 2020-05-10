68a912896107c34f46f2f5a0a26c60ffd979497a
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2020-05-10 22:33:25 +0300

    Pre complete

---
+++ semestrovaya/src/main/java/ru/itis/semestrovaya/model/Course.java
@@ -0,0 +1,28 @@
+package ru.itis.semestrovaya.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Lob
+    String name;
+    @Lob
+    String description;
+    String author;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "course")
+    @OrderBy("orderNumber")
+    List<Lesson> lessons;
+}
---
+++ semestrovaya/src/main/java/ru/itis/semestrovaya/service/CourseServiceImpl.java
@@ -0,0 +1,54 @@
+package ru.itis.semestrovaya.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import ru.itis.semestrovaya.config.security.details.UserDetailsImpl;
+import ru.itis.semestrovaya.model.Course;
+import ru.itis.semestrovaya.repository.CousresRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseServiceImpl implements CoursesService {
+
+    @Autowired
+    CousresRepository cousresRepository;
+    @Override
+    public List<Course> getAllCourses() {
+        return cousresRepository.findAll();
+    }
+
+    @Override
+    public Course getCourse(long id) {
+        Optional<Course> courseOptional = cousresRepository.findById(id);
+        return courseOptional.orElse(null);
+    }
+
+    @Override
+    public Course createEmptyCourse() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
+        Course newCourse = Course.builder()
+                .author(email)
+                .description("Empty description")
+                .name("New course")
+                .lessons(new ArrayList<>())
+                .build();
+        cousresRepository.save(newCourse);
+        return newCourse;
+    }
+
+    @Override
+    public void updateCourseById(long id, String name, String description) {
+        Course course = cousresRepository.findById(id).orElse(null);
+        if(course == null)
+            return;
+        course.setName(name);
+        course.setDescription(description);
+        cousresRepository.save(course);
+    }
+}
